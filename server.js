63600bf00a849cb0753a224b2b11581d9502305c
Janiththa <chalakajaniththa@gmail.com>
2024-08-25 13:21:27 +0530

    Create the .env file and configure the database connection with a separate file to improve the project structure.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const connectDB = require('./config/db');
 const app = express()
 
 //routes
@@ -12,13 +13,22 @@ app.get('/blog', (req, res) => {
     res.send('Hello Blog, This is my first express app.')
 })
 
-const uri = "mongodb+srv://admin:admin123@delivery-cost-calculato.jgdq6.mongodb.net/?retryWrites=true&w=majority&appName=Delivery-Cost-Calculator-Database";
+// const uri = "mongodb+srv://admin:admin123@delivery-cost-calculato.jgdq6.mongodb.net/?retryWrites=true&w=majority&appName=Delivery-Cost-Calculator-Database";
 
-mongoose.connect(uri).then(() => {
-    console.log('connected to MongoDB')
-    app.listen(3000, ()=> {
-        console.log('Node API app is running on port 3000')
-    });
-}).catch((error) => {
-    console.log(error)
-})
+// mongoose.connect(uri).then(() => {
+//     console.log('connected to MongoDB')
+//     app.listen(3000, ()=> {
+//         console.log('Node API app is running on port 3000')
+//     });
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+// Connect to MongoDB
+connectDB();
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
