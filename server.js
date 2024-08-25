28b0b89b3332a0de7a7e5eede6d7b1cc468b600b
Janiththa <chalakajaniththa@gmail.com>
2024-08-25 11:31:46 +0530

    resolved connection error and successfully established connection to MongoDB.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 //routes
@@ -7,6 +8,17 @@ app.get('/', (req, res) => {
     res.send('Hello Node API')
 })
 
-app.listen(3000, ()=> {
-    console.log('Node API app is running on port 3000')
+app.get('/blog', (req, res) => {
+    res.send('Hello Blog, This is my first express app.')
+})
+
+const uri = "mongodb+srv://admin:admin123@delivery-cost-calculato.jgdq6.mongodb.net/?retryWrites=true&w=majority&appName=Delivery-Cost-Calculator-Database";
+
+mongoose.connect(uri).then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, ()=> {
+        console.log('Node API app is running on port 3000')
+    });
+}).catch((error) => {
+    console.log(error)
 })
